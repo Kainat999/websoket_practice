57ff580f59a0c2390ada650775ae043b979bb7d9
kainat999 <kainatbajwah999@gmail.com>
2023-11-01 20:47:43 +0000

    add some updation

--- video_1_to_10/app/consumers.py
+++ video_1_to_10/app/consumers.py
@@ -21,3 +21,26 @@ class TestSyncConsumer(SyncConsumer):
     def websocket_disconnect(self, event):
         print('Websocket is Disconnected....', event) 
         raise StopConsumer  
+    
+
+
+
+class TestAyncConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        print('Websocket is Connected', event)
+        await self.send({
+            'type': 'websocket.accept',
+        })
+
+
+    async def websocket_receive(self, event):
+        print('Message is received........', event)   
+        print(event['text'])
+        await self.send({
+            'type': 'websocket.send',
+            'text': 'Message is Sended'
+        }) 
+
+    async def websocket_disconnect(self, event):
+        print('Websocket is Disconnected....', event) 
+        raise StopConsumer      
--- video_1_to_10/app/routing.py
+++ video_1_to_10/app/routing.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/test/', consumers.TestSyncConsumer.as_asgi())
+    path('ws/Stest/', consumers.TestSyncConsumer.as_asgi()),
+    path('ws/Atest/', consumers.TestAyncConsumer.as_asgi())
+
 ]
